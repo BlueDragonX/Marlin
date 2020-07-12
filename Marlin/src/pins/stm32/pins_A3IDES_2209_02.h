5d9ffb9de5971a526eb2e1d0dfdaf05de700c33a
Ryan Bourgeois <bluedragonx@gmail.com>
2020-07-11 21:22:45 -0700

    Replace TEMP_PINDA_PIN with TEMP_PROBE_PIN.

--- Marlin/src/pins/stm32/pins_A3IDES_2209_02.h
+++ Marlin/src/pins/stm32/pins_A3IDES_2209_02.h
@@ -112,7 +112,7 @@
 #define TEMP_0_PIN             (THERM_0_GPIO_Port + THERM_0_Pin)   // Analog Input
 #define TEMP_BED_PIN           (THERM_1_GPIO_Port + THERM_1_Pin)   // Analog Input
 
-#define TEMP_PINDA_PIN         PA6   // Analog Input //todo remove
+#define TEMP_PROBE_PIN         (THERM_PROBE_GPIO_Port + THERM_PROBE_Pin) // Analog Input
 #define TEMP_BOARD_PIN         (THERM_BOARD_GPIO_Port + THERM_BOARD_Pin) // Analog Input
 
 
